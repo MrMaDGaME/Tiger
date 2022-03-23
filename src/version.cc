f7e4176211ef690a7dc615a3a92224749bff9e9f
nicolas.muller <nicolas.muller@epita.fr>
2022-03-23 16:11:13 +0100

    Azy on essaye d'avoir une version qui print

--- src/version.cc
+++ src/version.cc
@@ -3,8 +3,9 @@
  ** \brief Common definitions.
  */
 
-#include <authors.h>
 #include <config.h>
+#include <authors.h>
+
 
 /*-------------------.
 | Program identity.  |
@@ -18,13 +19,16 @@ message.  */
 const char *program_name = PACKAGE_NAME;
 
 // Version string of this program.
-const char *program_version = "tc (" PACKAGE_STRING ")\n"
-                              "\n"
-                              "\n" PACKAGE_SHORT_AUTHORS;
+const char *program_version =
+"tc (" PACKAGE_STRING ")\n"
+"\n"
+"\n"
+PACKAGE_SHORT_AUTHORS;
 
 // Bug report address of this program.
 const char *program_bug_address = PACKAGE_BUGREPORT;
 
 /// Describe program and accepted arguments.
-const char *program_doc = "Tiger Compiler, Copyright (C) 2004-2021  LRDE.";
+const char *program_doc =
+  "Tiger Compiler, Copyright (C) 2004-2021  LRDE.";
 const char *program_args_doc = "INPUT-FILE";
